db93d1807c9d3052d03033827dba0b2c5f2ee7fb
Daniel Koelzer <DKoelzer01@gmail.com>
2024-05-30 00:44:33 -0500

    Initial BepInEx Plugin Setup

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+using BepInEx;
+using EFT.UI;
+using VisualQuestTree.UI;
+
+namespace VisualQuestTree
+{
+    [BepInPlugin("com.dewdongers.visualquesttree", "Visual Quest Tree", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Logger.LogInfo($"Visual Quest Tree loaded.");
+            QuestScreen.
+        }    
+    }
+}
---
+++ QuestScreen.cs
@@ -0,0 +1,15 @@
+using System;
+using EFT.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace VisualQuestTree.UI
+{
+    public class QuestScreen : MonoBehaviour
+    {
+        public static void Init()
+        {
+            
+        }
+    }
+}
