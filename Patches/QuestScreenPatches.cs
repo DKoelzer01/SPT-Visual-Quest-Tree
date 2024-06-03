78c0b7fe316603eaa5194c5c9004851d211c34d5
Daniel Koelzer <DKoelzer01@gmail.com>
2024-06-03 00:59:12 -0500

    Added Quest Tree toggling logic

--- Patches/QuestScreenPatches.cs
+++ Patches/QuestScreenPatches.cs
@@ -6,7 +6,9 @@ using Comfort.Common;
 using UnityEngine;
 using HarmonyLib;
 using VisualQuestTree.Utils;
+using VisualQuestTree.UI;
 using VisualQuestTree.UI.Controls;
+
 
 namespace VisualQuestTree.Patches
 {
@@ -22,20 +24,64 @@ namespace VisualQuestTree.Patches
         public static void Prefix(TasksScreen __instance)
         {
             Plugin.Log.LogInfo($"Task Screen Prefix");
-            if(Singleton<CommonUI>.Instance.transform.Find("Common UI/InventoryScreen/TasksScreen/QuestTreeButton") == null) {
+            if(Singleton<CommonUI>.Instance.transform.Find("Common UI/InventoryScreen/TasksScreen/QuestTreeButton").gameObject.name == null) {
                 var prefab = Singleton<CommonUI>.Instance.transform.Find("Common UI/InventoryScreen/BackButton").gameObject;
-                var go = UnityEngine.Object.Instantiate(prefab,Singleton<CommonUI>.Instance.transform.Find("Common UI/InventoryScreen/TasksScreen/QuestTreeBlock").gameObject.transform.parent,false);
+                var go = UnityEngine.Object.Instantiate(prefab,Singleton<CommonUI>.Instance.transform.Find("Common UI/InventoryScreen/Tasks Panel/QuestTreeBlock").gameObject.transform.parent,false);
                 QuestTreeButton = go.GetComponent<DefaultUIButton>();
 
                 go.name = "QuestTreeButton";
                 QuestTreeButton.SetHeaderText("Quest Tree", 24);
                 QuestTreeButton.SetRawText("Quest Tree",24);
                 
-                QuestTreeButton.Transform.position = new Vector3(1130,1350,0);
+                QuestTreeButton.Transform.position = new Vector3(1120,1355,0);
 
                 QuestTreeButton.OnClick.AddListener(() =>
                 {
-                    Plugin.Log.LogInfo($"OPEN QUEST TREE WOOHOO");
+                    foreach(Transform child in __instance.transform) 
+                    {
+                        if(QuestTreeButton.HeaderText == "Quest Tree") {
+                            switch(child.name)
+                            {   //Disable all children except QuestTreeBlock, ignore QuestTreeButton as it is how you return from QuestTreeBlock
+                                case("QuestTreeBlock"):
+                                child.gameObject.SetActive(true);
+                                break;
+                                case("QuestTreeButton"):
+                                break;
+                                default:
+                                child.gameObject.SetActive(false);
+                                break;
+                            }
+                        } else {
+                            switch(child.name)
+                            {   //Enable all children except QuestTreeBlock, Ignore QuestTreeButton,NotesImage,ExitButton, and NoteWindow. Set the Quest Items / Notes toggle to Quest Items.
+                                case("QuestTreeBlock"):
+                                child.gameObject.SetActive(false);
+                                break;
+                                case("QuestItemsToggleGroup"):
+                                child.gameObject.transform.GetChild(0).GetComponent<UIAnimatedToggleSpawner>().Boolean_0 = false;
+                                child.gameObject.SetActive(true);
+                                break;
+                                case("QuestTreeButton"):
+                                case("NotesImage"):
+                                case("ExitButton"):
+                                case("NoteWindow"):
+                                break;
+                                default:
+                                child.gameObject.SetActive(true);
+                                break;
+                            }
+                        }
+                        
+                    }
+
+                    if(QuestTreeButton.HeaderText == "Quest Tree") {
+                        Plugin.Instance.QuestsScreen.OpenQuestTree();
+                        QuestTreeButton.SetHeaderText("Task List", 24);
+                    } 
+                    else {
+                        Plugin.Instance.QuestsScreen.CloseQuestTree();
+                        QuestTreeButton.SetHeaderText("Quest Tree", 24);
+                    }
                 });
             }
         }
@@ -45,7 +91,6 @@ namespace VisualQuestTree.Patches
         {
             try
             {
-                Plugin.Log.LogInfo($"DEWEY Try Load Task Screen");
                 Plugin.Instance.QuestsScreen?.OnTaskScreenShow();
             }
             catch(Exception e)
@@ -69,7 +114,6 @@ namespace VisualQuestTree.Patches
         {
             try
             {
-                Plugin.Log.LogInfo($"DEWEY Try Close Task Screen");
                 Plugin.Instance.QuestsScreen?.OnTaskScreenClose();
             }
             catch(Exception e)
--- QuestScreen.cs
+++ QuestScreen.cs
@@ -19,45 +19,10 @@ namespace VisualQuestTree.UI
             return go.AddComponent<QuestTreeScreen>();
         }
 
-        private void Awake() 
-        {
-            Plugin.Log.LogInfo("Create QuestTreeToggle");
-            // var QuestTreeToggle = UIUtils.CreateUIGameObject(this.gameObject,"QuestTreeToggle");
-            // var QuestTreeToggleText = TextBox.Create(QuestTreeToggle, 12);
-            // QuestTreeToggle.transform.SetParent(this.transform);
-            // QuestTreeToggleText.SetText("QUEST TREE");
-            // QuestTreeToggle.transform.localPosition = new Vector3(1280,1350,0);
-
-
-        }
-
-        /*  Toggle the following items:
-                WhiteBackground
-                OverallBackground
-                QuestItemsToggleGroup
-                NotesImage
-                QuestItemsPanel
-                NotesPart
-                TasksPanel
-                NoteWindow
-        */
-
-        internal void OpenQuestTree()
+        public void OpenQuestTree()
         {
             Plugin.Log.LogInfo("Open Quest Tree");
-        }
-
-        internal void CloseQuestTree() 
-        {
-            Plugin.Log.LogInfo("Close Quest Tree");
-            // do the opposite of open
-        }
-        internal void OnTaskScreenShow()
-        {
-            Plugin.Log.LogInfo($"OpenTaskScreen");
-
-            transform.parent.gameObject.SetActive(true);
-
+            
             /*TODO: Draw UI for quest tree
                     Button for entering quest tree view;
                         Persist between screen switches
@@ -68,13 +33,22 @@ namespace VisualQuestTree.UI
                     Show all quests
                     Filter quests by Trader.
             */
+        }
+
+        public void CloseQuestTree() 
+        {
+            Plugin.Log.LogInfo("Close Quest Tree");
+            // do the opposite of open
+        }
+        internal void OnTaskScreenShow()
+        {
+            transform.parent.gameObject.SetActive(true);
+
             return;
         }
 
         internal void OnTaskScreenClose()
         {
-            Plugin.Log.LogInfo($"CloseTaskScreen");
-
             transform.parent.gameObject.SetActive(false);
         }
     }
